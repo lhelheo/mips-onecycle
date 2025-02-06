1ccdacebbee97c7f27dca0cb285c095aac313a1f
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2025-02-06 11:48:30 -0300

    fix: control unit

---
+++ ControlUnit.v
@@ -0,0 +1,85 @@
+module ControlUnit (
+    input wire [5:0] Opcode,
+    output reg RegDst, 
+    output reg ALUSrc,
+    output reg MemtoReg,
+    output reg RegWrite,
+    output reg MemRead,
+    output reg MemWrite,
+    output reg Branch,
+    output reg [1:0] ALUOp,
+    output reg Jump
+);
+
+    always @(*) begin
+      case (Opcode)
+        6'b000000: begin // R-Type
+            RegDst = 1;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 1;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b10;
+            Jump = 0;
+        end
+        6'b100011: begin // lw
+            RegDst = 0;
+            ALUSrc = 1;
+            MemtoReg = 1;
+            RegWrite = 1;
+            MemRead = 1;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+        6'b101011: begin // sw
+            RegDst = 0;
+            ALUSrc = 1;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 1;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+        6'b000100: begin // beq
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 1;
+            ALUOp = 2'b01;
+            Jump = 0;
+        end
+        6'b000010: begin // jump
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 1;
+        end
+        default: begin // Instrução inválida
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+    endcase
+end
+
+endmodule
