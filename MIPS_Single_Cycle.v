9df852c5e1433486069845e6862aeeb7a87928bb
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2025-02-06 02:48:35 -0300

    feat: alu and control unit

--- MIPS_Single_Cycle.v
+++ MIPS_Single_Cycle.v
@@ -57,3 +57,115 @@ module MIPS_Single_Cycle(
     );
 
 endmodule
+
+module ControlUnit (
+    input wire [5:0] Opcode,
+    output reg RegDst, 
+    output reg ALUSrc,
+    output reg MemtoReg,
+    output reg RegWrite,
+    output reg MemRead,
+    output reg MemWrite,
+    output reg Branch,
+    output reg [1:0] ALUOp,
+    output reg Jump
+);
+
+    always @(*) begin
+      case (Opcode)
+        6'b000000: begin // R-Type
+            RegDst = 1;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 1;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b10;
+            Jump = 0;
+        end
+        6'b100011: begin // lw
+            RegDst = 0;
+            ALUSrc = 1;
+            MemtoReg = 1;
+            RegWrite = 1;
+            MemRead = 1;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+        6'b101011: begin // sw
+            RegDst = 0;
+            ALUSrc = 1;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 1;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+        6'b000100: begin // beq
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 1;
+            ALUOp = 2'b01;
+            Jump = 0;
+        end
+        6'b000010: begin // jump
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 1;
+        end
+        default: begin // Instrução inválida
+            RegDst = 0;
+            ALUSrc = 0;
+            MemtoReg = 0;
+            RegWrite = 0;
+            MemRead = 0;
+            MemWrite = 0;
+            Branch = 0;
+            ALUOp = 2'b00;
+            Jump = 0;
+        end
+    endcase
+end
+
+endmodule
+
+module ALUControl (
+    input wire [1:0] ALUOp,
+    input wire [5:0] Funct,
+    output reg [3:0] ALUOperation
+);
+
+    always @(*) begin
+        case (ALUOp)
+            2'b00: ALUOperation = 4'b0010; // soma lw/sw
+            2'b01: ALUOperation = 4'b0110; // sub beq
+            2'b10: begin
+                case (Funct)
+                    6'b100000: ALUOperation = 4'b0010; // add
+                    6'b100010: ALUOperation = 4'b0110; // sub
+                    6'b100100: ALUOperation = 4'b0000; // and
+                    6'b100101: ALUOperation = 4'b0001; // or
+                    6'b101010: ALUOperation = 4'b0111; // slt
+                    default: ALUOperation = 4'b0000; // Instrução inválida
+                endcase
+            end
+            default: ALUOperation = 4'b0000; // Instrução inválida
+        endcase
+    end
+
+endmodule
